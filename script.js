6b55abd8e0444879e442ad3911674bb40bba16ec
vandita23 <vanditalall23@gmail.com>
2026-03-14 11:39:22 +0530

    Implement star animation on canvas
    Added star animation to the canvas with scrolling effect.

--- script.js
+++ script.js
@@ -84,25 +84,42 @@ scrollSpeed = window.scrollY * 0.02;
 
 });
 
-// animate stars
+
+const canvas = document.getElementById("universe");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+
+for(let i=0;i<600;i++){
+
+stars.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2,
+speed:Math.random()*0.4 + 0.1
+});
+
+}
+
 function animateStars(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
-stars.forEach(star => {
+stars.forEach(star=>{
 
-star.y += star.size + scrollSpeed * 0.05;
+star.y += star.speed;
 
 if(star.y > canvas.height){
-
 star.y = 0;
-star.x = Math.random() * canvas.width;
-
+star.x = Math.random()*canvas.width;
 }
 
 ctx.beginPath();
-ctx.arc(star.x, star.y, star.size, 0, Math.PI*2);
-ctx.fillStyle = "white";
+ctx.arc(star.x,star.y,star.size,0,Math.PI*2);
+ctx.fillStyle="white";
 ctx.fill();
 
 });
@@ -112,23 +129,6 @@ requestAnimationFrame(animateStars);
 }
 
 animateStars();
-
-const sections = document.querySelectorAll(".story");
-
-window.addEventListener("scroll",()=>{
-
-sections.forEach(sec=>{
-
-const top = sec.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-sec.classList.add("visible");
-}
-
-});
-
-});
-
 
 
 // METEORS
